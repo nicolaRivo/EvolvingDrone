8135445f5bb394329ad002d1edab82f5f49d8915
nicolaRivo <nicola.rivosecchi@gmail.com>
2022-11-06 11:51:24 +0000

    improved documentation and readability of functions

--- Source/FilterSynth.h
+++ Source/FilterSynth.h
@@ -14,47 +14,56 @@
 class FilterSynth
 {
 public:
+    
+    ///set the sample rate to the lfo and to the squareWave oscillator
     void setSampleRate(float sr)
     {
         sampleRate = sr;
         squareOsc.setSampleRate(sr);
-        squareOsc.setFrequency(oscFrequency);
+        squareOsc.setFrequency(oscFrequency); //not sure wether this is necessary?
         lfo.setSampleRate(sr);
-        lfo.setFrequency(lfoFrequency);
+        lfo.setFrequency(lfoFrequency);  //not sure wether this is necessary?
     }
     
+    ///set the lfo frequency
     void setLfoFrequency(float f)
     {
         lfoFrequency = f;
         lfo.setFrequency(lfoFrequency);
     }
     
+    ///set the square wave oscillator frequency
     void setOscFrequency(float f)
     {
         oscFrequency = f;
         squareOsc.setFrequency(oscFrequency);
     }
     
+    ///set the resonance for the filter
     void setResonance(float r)
     {
         resonance = r;
     }
     
+    ///set the minimum cutoff point for the oscillating filter
     void setMinCutoff(float mco)
     {
         minCutoff = mco;
     }
 
+    ///set the maximum cutoff point for the oscillating filter
     void setMaxCutoff(float mco)
     {
         maxCutoff = mco;
     }
     
+    ///set the gain for the filter
     void setGain(float g)
     {
         gain = g;
     }
     
+    ///randomize all the parameters
     void randomizeParams()
     {
         setOscFrequency(random.nextFloat() * 2000.0f);
@@ -65,12 +74,19 @@ public:
         setGain (random.nextFloat() * 0.9f);
     }
     
+    ///generate a filtered square wave sample
     float process ()
     {
-        float cutoff = ((lfo.process()/2) + 0.5f) * (maxCutoff - minCutoff) + minCutoff;
-        filter.setCoefficients(juce::IIRCoefficients::makeLowPass(sampleRate, cutoff, resonance));
-        float rawSquareWave = squareOsc.process() * gain ;//generate the required sample
-        float filteredSquare = filter.processSingleSampleRaw(rawSquareWave);
+        float processedLfo =lfo.process(); //lfo oscillating between ±1
+        float positiveLfo =(processedLfo/2) + 0.5f) //shift the lfo range between 0 and 1 (negative cutoff values would break the filter)
+        float cutoff = positiveLfo * (maxCutoff - minCutoff) + minCutoff;//map the oscillating cutoff to the desired range
+        
+        filter.setCoefficients(juce::IIRCoefficients::makeLowPass(sampleRate, cutoff, resonance)); //set the filter
+        
+        float rawSquareWave = squareOsc.process() * gain ;//generate the required square wave sample
+        
+        float filteredSquare = filter.processSingleSampleRaw(rawSquareWave); //apply the filter to the square wave
+        
         return filteredSquare;
     }
     
